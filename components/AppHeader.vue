05e40c014dd2294c812cc029627143decad153fd
Vitaly <vytalymedvedev@gmail.com>
2023-10-23 18:50:31 +0600

    Add header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,14 @@
-<template>
-  Header
+<template lang="pug">
+header(class="header center-items")
+  img(class="logo-max-width" src="/Logo.svg")
 </template>
+<style scoped lang="scss">
+@use "~/assets/scss/main.scss";
+.header {
+  padding: 1.65rem;
+}
+
+.logo-max-width {
+  max-width: 8.3rem;
+}
+</style>
