53719fb4a19c559dff26d266e16b3abb9b69fbb3
Vitaly <vytalymedvedev@gmail.com>
2023-10-26 11:54:21 +0600

    Add google fonts package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [['@nuxtjs/google-fonts', {
+    families: {
+      Montserrat: [400, 700],
+    },
+  }
+  ]],
   devtools: { enabled: true },
   vite: {
     css: {
